820c9052aa4bdaa4dae5b77edaa2bd0319a341c3
Shaylah Fleming <68482745+shayflem@users.noreply.github.com>
2020-09-25 12:01:15 -0400

    shell.cpp

---
+++ .github/workflows/blank.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namspace std;
+
+int a,b,c;
+
+cout << "Enter value 'a' as a whole number" << endl;
+cin >> a;
+
+cout << "Enter value 'b' as a whole number" << endl;
+cin >> b;
+
+c=a+b;
+
+
+return 0;
